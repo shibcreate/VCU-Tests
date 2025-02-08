103846f029647fdf74d24f2033a1ff21d98a45a1
harleen <harleensandhu977@gmail.com>
2025-02-08 11:50:49 -0800

    Revert "changed pid calculation to orignal value"
    This reverts commit 737631754ff7b1daed5ae0746392a443b85306cc.

--- dev/PID.c
+++ dev/PID.c
@@ -66,22 +66,17 @@ void PID_updateSetpoint(PID *pid, sbyte2 setpoint) {
 
 void PID_computeOutput(PID *pid, sbyte2 sensorValue) {
     sbyte2 currentError = pid->setpoint - sensorValue;
-    pid->Kp = (sbyte1)pid->Kp/10;
-    pid->Ki = (sbyte1)pid->Ki/10;
-    pid->Kd = (sbyte1)pid->Kd/10;
-
     pid->proportional   = -28;//(sbyte2) pid->Kp * currentError / 10;
-    pid->integral       = (sbyte2) ( (sbyte4) pid->Ki * (pid->totalError + currentError) * pid->dH );
-    pid->derivative     = (sbyte2) pid->Kd * (currentError - pid->previousError) * pid->dH ;
+    pid->integral       = (sbyte2) ( (sbyte4) pid->Ki * (pid->totalError + currentError) / pid->dH  / 10 );
+    pid->derivative     = (sbyte2) pid->Kd * (currentError - pid->previousError) * pid->dH  / 10;
     pid->previousError  = currentError;
-    pid->totalError    += (sbyte4)currentError* pid->dH;
+    pid->totalError    += (sbyte4)currentError;
 
     // At minimum, a P(ID) Controller will always use Proportional Control
-    pid->output = pid->proportional+pid->integral+pid->derivative;
-
+    pid->output = pid->proportional;
 
     //Check to see if motor is saturated at max torque request already
-    /*if(pid->saturationValue >= sensorValue)
+    if(pid->saturationValue >= sensorValue)
     {
         pid->antiWindupFlag = FALSE;
         pid->output += pid->integral;
@@ -91,7 +86,6 @@ void PID_computeOutput(PID *pid, sbyte2 sensorValue) {
         pid->antiWindupFlag = TRUE;
 
     //return pid->output;
-    */
 }
 
 /** GETTER FUNCTIONS **/
